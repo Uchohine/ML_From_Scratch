8729d61b50d854dc4649af7eff00a07470358972
Uchohine <wxm1210@vt.edu>
2021-06-17 13:03:42 +0800

    finish Adaboost

--- Algorithm/Boost/Adaboost.py
+++ Algorithm/Boost/Adaboost.py
@@ -1,33 +1,73 @@
 import numpy as np
 from Algorithm import util
 
+from sklearn.datasets import load_iris
+import time
+
 
 class Adaboost():
 	def __init__(self, terminal_iter = 100, classifier = 'decision_tree'):
-		self.classifier = util.Get_Classifer(classifier)
-		self.alpha = np.array(list())
+		self.classifier = classifier
+		self.classifiers = list()
+		self.alpha = list()
 		self.terminal = terminal_iter
 
-	def fit(self,x,y):
+	def fit(self, x, y):
 		self.weights = np.ones(x.shape[0]) / x.shape[0]
 		for i in range(self.terminal):
-			idx = np.random.choice(self.weights.shape[0], self.weights.shape[0], p = self.weights)
-
+			idx = np.random.choice(self.weights.shape[0], self.weights.shape[0], p=self.weights)
 			x_train = x[idx]
 			y_train = y[idx]
 
-			self.classifier.fit(x_train, y_train)
-			ypred = self.classifier.predict(x_train)
+			model = util.Get_Classifer(self.classifier)
+			model.fit(x_train, y_train)
+			ypred = model.predict(x_train)
+			err = (ypred != y_train)
 
-			epi = np.sum((ypred != y_train) * self.weights)
+			epi = np.matmul(self.weights.T, err)
+			if epi == .5 or epi == 0:
+				break
 
+			alpha = .5 * np.log((1/epi) - 1)
+			self.alpha.append(alpha)
+			self.classifiers.append(model)
+
+			self.weights *= np.exp(-alpha * (err + np.finfo(float).eps))
+			self.weights /= np.sum(self.weights)
+		self.alpha = np.array(self.alpha)
+		self.alpha = self.alpha / np.sum(self.alpha)
+
+	def predict(self, x):
+		y = list()
+		for i in range(len(self.alpha)):
+			y.append(self.classifiers[i].predict(x))
+		y = np.array(y)
+		return np.rint(np.matmul(y.T, self.alpha))
 
 	def __str__(self):
-		return self.alpha.__str__()
+		return np.array2string(self.alpha)
 
 
 
 
 if __name__ == '__main__':
-	c = Adaboost()
-	print(c)
+	data = load_iris()
+	x, y, col = data['data'], data['target'], data['feature_names']
+
+	from sklearn.model_selection import train_test_split
+
+	X_train, X_val, y_train, y_val = train_test_split(x, y, random_state=44)
+
+	model = Adaboost()
+
+	start = time.time()
+	model.fit(X_train, y_train)
+	end = time.time()
+	print((end - start) * 100)
+
+	from sklearn.metrics import accuracy_score
+	y_pred = model.predict(X_val)
+	print(y_pred)
+	print(y_val)
+	print(f'Accuracy for self built model {accuracy_score(y_val, y_pred)}')
+
--- Algorithm/util.py
+++ Algorithm/util.py
@@ -1,10 +1,36 @@
 from Algorithm.Tree.decision_tree import decision_tree
 
-def Get_Classifer (name = ''):
-	switcher = {
-		'decision_tree':decision_tree
-	}
-	res = switcher.get(name)
-	if res == None:
-		raise Exception('No Criterion Named: "{name}".'.format(name = name));
-	return res
+from sklearn.datasets import load_iris
+import time
+
+
+def Get_Classifer(name=''):
+    switcher = {
+        'decision_tree': decision_tree
+    }
+    res = switcher.get(name)
+    if res == None:
+        raise Exception('No Criterion Named: "{name}".'.format(name=name));
+    return res()
+
+
+if __name__ == '__main__':
+    data = load_iris()
+    x, y, col = data['data'], data['target'], data['feature_names']
+
+    from sklearn.model_selection import train_test_split
+
+    X_train, X_val, y_train, y_val = train_test_split(x, y, random_state=44)
+    print(X_train.shape)
+    model = Get_Classifer('decision_tree')
+
+    start = time.time()
+    model.fit(X_train,y_train)
+    end = time.time()
+    print((end - start) * 100)
+
+    from sklearn.metrics import accuracy_score
+
+    y_pred = model.predict(X_val)
+    print(y_pred)
+    print(f'Accuracy for self built model {accuracy_score(y_val, y_pred)}')
